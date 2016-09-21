dab6d6b93d051cfa7c380638e2ae321380a1f925
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-09-21 15:07:36 -0400

    Added models to admin

--- root/posts/admin.py
+++ root/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Post, Vote, Comment
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Vote)
+admin.site.register(Comment)
