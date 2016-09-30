15216590238c9401a0d9c0792c2e55ec2adf0d16
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-09-30 13:16:32 -0400

    Adding views and url

--- root/posts/views.py
+++ root/posts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index page")
+    posts = Post.objects.order_by('posted_on')
+    context = { 'posts': posts }
+    return render(request, 'posts/index.html', context)
+
+def post(request,id):
+    post = get_object_or_404(Post,id)
+    context = { 'post': post }
+    return render(request, 'posts/detail.html', context)
+    
